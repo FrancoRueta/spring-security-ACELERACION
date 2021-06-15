33ce45e631f7759dd3e9821d17838643cf6b29c3
FrancoRueta <askjfuhje@hotmail.com>
2021-06-14 22:14:34 -0300

    commit inicial.

---
+++ src/main/java/com/franco/securityACELERACION/SecurityAceleracionApplication.java
@@ -0,0 +1,13 @@
+package com.franco.securityACELERACION;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAceleracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAceleracionApplication.class, args);
+	}
+
+}
