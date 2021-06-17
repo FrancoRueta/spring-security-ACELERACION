3cbf17c1076a8fcfcff94cdb9ea2fc47adf762bb
FrancoRueta <askjfuhje@hotmail.com>
2021-06-17 20:43:56 -0300

    ADDITIONS: *Email sending fully configured.
    ERRORS:
    *Not working.

---
+++ src/main/java/com/franco/securityACELERACION/email/EmailService.java
@@ -0,0 +1,44 @@
+package com.franco.securityACELERACION.email;
+
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+//@AllArgsConstructor
+public class EmailService implements EmailSender{
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    @Async
+    public void send(String to, String email) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirmacion del correo.");
+            helper.setFrom("security.alkemy@alkemy.com");
+        }
+        catch(MessagingException e) {
+            LOGGER.error("Error al enviar el email", e);
+            throw new IllegalStateException("Error al enviar el email.");
+        }
+    }
+}
